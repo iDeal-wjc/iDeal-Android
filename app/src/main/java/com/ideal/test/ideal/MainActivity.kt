bf57bc01eaeade572361f64a7d811ad4a6efc53c
Jiechao Wang <jiechao@starmobileinc.com>
2018-06-28 01:29:19 -0400

    no message

--- app/src/main/java/com/ideal/test/ideal/MainActivity.kt
+++ app/src/main/java/com/ideal/test/ideal/MainActivity.kt
@@ -4,23 +4,41 @@ import android.annotation.TargetApi
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.amap.api.location.AMapLocation
 import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationListener
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val SDK_PERMISSION_REQUEST = 1
-    //声明AMapLocationClient类对象
+    companion object {
+        private const val SDK_PERMISSION_REQUEST = 1
+    }
+
     private var locationClient: AMapLocationClient? = null
     private var aMapLocation: AMapLocation? = null
+    private var permissionsGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         checkPermissions()
+
+        button_locate.setOnClickListener { _ ->
+            if (! permissionsGranted) return@setOnClickListener
+            val mAMapLocationListener = AMapLocationListener { location ->
+                if (location.errorCode == 0) {
+                    aMapLocation = location
+                    Toast.makeText(this, location.toString(), Toast.LENGTH_LONG).show()
+                    locationClient?.stopLocation()
+                }
+            }
+            locationClient = AMapLocationClient(applicationContext)
+            locationClient?.setLocationListener(mAMapLocationListener)
+            locationClient?.startLocation()
+        }
     }
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -30,7 +48,8 @@ class MainActivity : AppCompatActivity() {
         if (permission.isNotEmpty()) {
             // 申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义）
             requestPermissions(permission.toTypedArray(), SDK_PERMISSION_REQUEST)
-            return
+        } else {
+            permissionsGranted = true
         }
     }
 
@@ -38,19 +57,7 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             SDK_PERMISSION_REQUEST -> {
-                //异步获取定位结果
-                val mAMapLocationListener = AMapLocationListener { location ->
-                    if (location != null && location.errorCode == 0) {
-                        aMapLocation = location
-                    }
-                }
-
-                //初始化定位
-                locationClient = AMapLocationClient(applicationContext)
-                //设置定位回调监听
-                locationClient?.setLocationListener(mAMapLocationListener)
-                //启动定位
-                locationClient?.startLocation()
+                permissionsGranted = true
             }
         }
     }
